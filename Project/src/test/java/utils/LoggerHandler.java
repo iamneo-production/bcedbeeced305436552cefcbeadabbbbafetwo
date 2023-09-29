45dbf00a9c61fec1f12c95872184e72806aabe0b
Iamneo <techadmin@iamneo.ai>
2023-09-29 09:39:43 +0000

    Backup Commit

--- Project/src/test/java/utils/LoggerHandler.java
+++ Project/src/test/java/utils/LoggerHandler.java
@@ -16,23 +16,12 @@ public class LoggerHandler {
     }
 
     public static void initLog4j() {
-        // // Set the system property for currentTimestamp
-        // SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
-        // String timestamp = sdf.format(new Date());
-        // System.setProperty("log.datePattern", "yyyy-MM-dd-HH-mm-ss");
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
+    String timestamp = sdf.format(new Date());
+    System.setProperty("log.timestamp", timestamp);
 
-        // Initialize Log4j
-        PropertyConfigurator.configure("src/main/resources/log4j.properties");
-
-        // // Obtain the "file" appender and configure it
-        // Logger log = Logger.getLogger(LoggerHandler.class);
-        // DailyRollingFileAppender appender = (DailyRollingFileAppender) log.getAppender("file");
-
-        // // Configure the appender as needed
-        // appender.setFile("logs/main_" + timestamp + ".log");
-        // PatternLayout layout = new PatternLayout();
-        // layout.setConversionPattern("%d %d{Z} [%t] %-5p (%F:%L) - %m%n");
-        // appender.setLayout(layout);
-        // appender.activateOptions(); 
+    // Initialize Log4j
+    PropertyConfigurator.configure("src/main/resources/log4j.properties");
 }
+
 }
