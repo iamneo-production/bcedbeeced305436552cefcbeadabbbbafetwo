3001711a8a427e76f8e172280f3a4fdcad59ecfc
Iamneo <techadmin@iamneo.ai>
2023-09-29 09:34:06 +0000

    Backup Commit

--- Project/src/test/java/utils/LoggerHandler.java
+++ Project/src/test/java/utils/LoggerHandler.java
@@ -16,23 +16,23 @@ public class LoggerHandler {
     }
 
     public static void initLog4j() {
-        // Set the system property for currentTimestamp
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
-        String timestamp = sdf.format(new Date());
-        System.setProperty("log.datePattern", "yyyy-MM-dd-HH-mm-ss");
+        // // Set the system property for currentTimestamp
+        // SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
+        // String timestamp = sdf.format(new Date());
+        // System.setProperty("log.datePattern", "yyyy-MM-dd-HH-mm-ss");
 
         // Initialize Log4j
         PropertyConfigurator.configure("src/main/resources/log4j.properties");
 
-        // Obtain the "file" appender and configure it
-        Logger log = Logger.getLogger(LoggerHandler.class);
-        DailyRollingFileAppender appender = (DailyRollingFileAppender) log.getAppender("file");
+        // // Obtain the "file" appender and configure it
+        // Logger log = Logger.getLogger(LoggerHandler.class);
+        // DailyRollingFileAppender appender = (DailyRollingFileAppender) log.getAppender("file");
 
-        // Configure the appender as needed
-        appender.setFile("logs/main_" + timestamp + ".log");
-        PatternLayout layout = new PatternLayout();
-        layout.setConversionPattern("%d %d{Z} [%t] %-5p (%F:%L) - %m%n");
-        appender.setLayout(layout);
-        appender.activateOptions(); // Activate the new options
-    }
+        // // Configure the appender as needed
+        // appender.setFile("logs/main_" + timestamp + ".log");
+        // PatternLayout layout = new PatternLayout();
+        // layout.setConversionPattern("%d %d{Z} [%t] %-5p (%F:%L) - %m%n");
+        // appender.setLayout(layout);
+        // appender.activateOptions(); 
+}
 }
